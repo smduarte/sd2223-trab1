ce68848687b8bf15273419cc3226ed7c41dd28eb
anaGadelha19 <a.gadelha@campus.fct.unl.pt>
2023-06-03 03:15:01 +0100

    mastodon cont

--- src/sd2223/trab2/servers/mastodon/Mastodon.java
+++ src/sd2223/trab2/servers/mastodon/Mastodon.java
@@ -38,10 +38,11 @@ public class Mastodon implements Feeds {
 
     private static final String clientKey = "7sfncEuTWxzLCnwQ1QKSaDKCvW4TCm5r-WsRHXcPJrM";
     private static final String clientSecret = "OOwaxMqAN0KV-pgLvZSxhUR0Qdf7_RrxcWto7XSNaA4";
-    private static final String accessTokenStr = "_08uoZNcGT3wwofiE6XLIwopV0tUUdPMxz_CbuenW3Q";
+    private static final String accessTokenStr = "SWgW04nCA6cZPcaAc433TIL-p3JiRP1UWWK8Ti3mI98";
 
 
     static final String STATUSES_PATH = "/api/v1/statuses";
+    static final String STATUSES_PATH_ID = "/api/v1/statuses/%d";
     static final String TIMELINES_PATH = "/api/v1/timelines/home";
     static final String ACCOUNT_FOLLOWING_PATH = "/api/v1/accounts/%s/following";
     static final String VERIFY_CREDENTIALS_PATH = "/api/v1/accounts/verify_credentials";
@@ -89,6 +90,7 @@ public class Mastodon implements Feeds {
             service.signRequest(accessToken, request);
 
             Response response = service.execute(request);
+            System.out.println(response.getCode());
             if (response.getCode() == HTTP_OK) {
                 var res = JSON.decode(response.getBody(), PostStatusResult.class);
                 return ok(res.getId());
@@ -125,11 +127,16 @@ public class Mastodon implements Feeds {
     public Result<Void> removeFromPersonalFeed(String user, long mid, String pwd) {
 
         try {
-            final OAuthRequest request = new OAuthRequest(Verb.DELETE, getEndpoint(STATUSES_PATH + "/%d", mid));
+            final OAuthRequest request = new OAuthRequest(Verb.DELETE, getEndpoint(STATUSES_PATH_ID, mid));
+
+
             service.signRequest(accessToken, request);
 
             Response response = service.execute(request);
+
             if (response.getCode() == HTTP_OK) {
+
+
                 return ok();
             }
 
